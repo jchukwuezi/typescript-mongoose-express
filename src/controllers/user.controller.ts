df0da1c408f27300e9ec63a719ad63f80b7311dd
jchukwuezi <jchukwuezi@gmail.com>
2024-10-24 19:58:02 +0100

    added login logic and route

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,10 +2,7 @@ import { Request, Response } from "express";
 import { User } from "../models/user.model";
 import bcrypt from "bcrypt";
 
-const registerUser = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
+const registerUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const { name, email, password } = req.body;
 
@@ -40,12 +37,28 @@ const registerUser = async (
   }
 };
 
-const loginUser = async (
-  req: Request,
-  res: Response
-): Promise<void> => {};
+const loginUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      res.status(401).json({ message: "Invalid password" });
+    }
+
+    res.status(200).json({ message: "Login successfull" });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in" });
+  }
+};
 
 export const userController = {
-    registerUser,
-    loginUser
-}
+  registerUser,
+  loginUser,
+};
--- src/routes/api/user.routes.ts
+++ src/routes/api/user.routes.ts
@@ -4,5 +4,6 @@ import { userController } from "../../controllers/user.controller";
 const router = Router()
 
 router.post('/register', userController.registerUser)
+router.post('/login', userController.loginUser)
 
 export default router;
